b4013efb347259d084459adb09ef782d3598095a
noae123 <noae123@gmail.com>
2022-05-23 10:55:44 +0300

    changing the colors, again....

--- static/javascript/template.js
+++ static/javascript/template.js
@@ -2,7 +2,7 @@ let myP;
 
 function loadAnime(){
     myP = document.querySelectorAll(".player");
-    animateElement(myP[0], false, "red", "butterfly");
+    animateElement(myP[0], false, "pink", "butterfly");
     animateElement(myP[1], false, "brown", "circle");
     animateElement(myP[2], false, "white", "heart");
     animateElement(myP[3], false, "blue", "tear");
