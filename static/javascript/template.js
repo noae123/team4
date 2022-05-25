d691dbe216867faf3264d521b07f676adfc5a698
noae123 <noae123@gmail.com>
2022-05-25 16:10:25 +0300

    Are we succefully done?

--- static/javascript/template.js
+++ static/javascript/template.js
@@ -1,6 +1,16 @@
 let myP;
+let password;
+let confirm_password;
 
 function loadAnime(){
+    password = document.getElementById("inputPassword")
+    confirm_password = document.getElementById("inputConfirmPassword");
+
+    if(password!==null){
+        password.onchange = validatePassword;
+        confirm_password.onkeyup = validatePassword;
+    }
+
     myP = document.querySelectorAll(".player");
     animateElement(myP[0], false, "pink", "butterfly");
     animateElement(myP[1], false, "brown", "circle");
@@ -16,3 +26,14 @@ function changeDisplay(){
     }
     document.body.classList.toggle("previewClosed")
 }
+
+/*adding invalid password check*/
+function validatePassword(){
+        if(password.value != confirm_password.value) {
+            confirm_password.setCustomValidity("Passwords Don't Match");
+        } else {
+            confirm_password.setCustomValidity('');
+        }
+}
+
+
