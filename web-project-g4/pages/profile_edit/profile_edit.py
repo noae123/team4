a0f87df22456881492661e7d897f27fd7766beaf
noae123 <noae123@gmail.com>
2022-07-18 08:39:46 +0300

    making some changes so it will mostly work

--- web-project-g4/pages/profile_edit/profile_edit.py
+++ web-project-g4/pages/profile_edit/profile_edit.py
@@ -1,10 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,session, url_for, redirect
 
 # homepage blueprint definition
 profile_edit = Blueprint('profile_edit', __name__, static_folder='static', static_url_path='/profile_edit', template_folder='templates')
 
 
 # Routes
-@profile_edit.route('/profile_edit')
+@profile_edit.route('/profile_edit/') #user must come from session
 def index():
-    return render_template('profile_edit.html')
+    session["logedIn"] = True
+    if ("logedIn" in session and session['logedIn'] == True):
+        # if(VIDEO_ID == None):
+        video_info = {
+            'audio': url_for('audio_player.static', filename='media/music/happybday.mp3'),
+            'img': url_for('audio_player.static', filename='media/img/Black.png'),
+            'sender_name': 'Sender Name',
+            'card_banner': 'Card Banner',
+            'color': 'white',
+            'shape': 'circle',
+            'dir': False
+        }
+
+        # else go to the db and get me the video
+
+        # make this to some kind of string
+        VIDEO_ID = 'p' + str(0) #need to change and give here a list of videos
+        return render_template('profile_edit.html', video_id=VIDEO_ID, video_info=video_info)
+    else:
+        return redirect(url_for('login.index'))
--- web-project-g4/pages/published/published.py
+++ web-project-g4/pages/published/published.py
@@ -1,10 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, url_for, redirect
 
 # homepage blueprint definition
 published = Blueprint('published', __name__, static_folder='static', static_url_path='/published', template_folder='templates')
 
 
 # Routes
-@published.route('/published')
-def index():
-    return render_template('published.html')
+@published.route('/published/<int:VIDEO_ID>') #user must come from session
+def index(VIDEO_ID: int):
+    session["logedIn"] = True
+    if ("logedIn" in session and session['logedIn'] == True):
+        # if(VIDEO_ID == None):
+        video_info = {
+            'audio': url_for('audio_player.static', filename='media/music/happybday.mp3'),
+            'img': url_for('audio_player.static', filename='media/img/Black.png'),
+            'sender_name': 'Sender Name',
+            'card_banner': 'Card Banner',
+            'color': 'white',
+            'shape': 'circle',
+            'dir': False
+        }
+
+        # else go to the db and get me the video
+
+        # make this to some kind of string
+        VIDEO_ID = 'p' + str(0) #need to change and give here a list of videos
+        return render_template('published.html', video_id=VIDEO_ID, video_info=video_info)
+    else:
+        return redirect(url_for('login.index'))
