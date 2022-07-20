ff9654c046dc55928f2627a1a2ff405811d46716
EdenLevy22 <103043688+EdenLevy22@users.noreply.github.com>
2022-07-20 21:20:15 +0300

    update

--- web-project-g4/utilities/db/users.py
+++ web-project-g4/utilities/db/users.py
@@ -12,10 +12,22 @@ def get_user(user_name):
     users_list = db_man.fetch(query)
     bool_ans=True
     if users_list==[]:
-        bool_ans=False
-    print(users_list)
-    print(bool_ans)
+        bool_ans = False
+    # print(users_list)
+    # print(bool_ans)
     return (users_list, bool_ans)
 
-get_user('Eden')
+#get_user('Eden')
 
+def create_user(name,user_nickname,user_password,user_email):
+    bool_ans=True
+    if get_user(name)[1] == True:
+       #print('user exist')
+        bool_ans = False
+    else:
+        query = "Insert into customers (user_name,nickname,password,email) VALUES ( '%s', '%s', '%s', '%s')" % (name, user_nickname, user_password, user_email)
+        db_man.commit(query)
+    return(bool_ans)
+
+
+#create_user('Gil','gili2202','Don12345','don@gmail.com')
