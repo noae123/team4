e0224f47b7e7a5eae51dc85b7485f572f4f5097d
noae123 <noae123@gmail.com>
2022-07-21 12:11:26 +0300

    merging aviv and master

--- web-project-g4/utilities/db/users.py
+++ web-project-g4/utilities/db/users.py
@@ -10,14 +10,14 @@ def get_user_by_name(user_name):
 
 def get_user_by_id(id):
     query = "select * from customers where id='%s';" % id
-    user_row = db_man.fetch(query)
+    user_row = dbManager.fetch(query)
     bool_ans = True
     if user_row == []:
         bool_ans = False
-    user_name=db_man.fetch("select user_name from customers where id='%s';"%id)
-    user_nickname=db_man.fetch("select nickname from customers where id='%s';"%id)
-    user_email=db_man.fetch("select email from customers where id='%s';"%id)
-    user_password=db_man.fetch("select password from customers where id='%s';"%id)
+    user_name=dbManager.fetch("select user_name from customers where id='%s';"%id)
+    user_nickname=dbManager.fetch("select nickname from customers where id='%s';"%id)
+    user_email=dbManager.fetch("select email from customers where id='%s';"%id)
+    user_password=dbManager.fetch("select password from customers where id='%s';"%id)
     return (user_name,user_nickname,user_email, user_password, bool_ans)
 
 
@@ -27,7 +27,7 @@ def create_user(name,user_nickname,user_password,user_email):
         bool_ans = False
     else:
         query = "Insert into customers (user_name,nickname,password,email) VALUES ( '%s', '%s', '%s', '%s')" % (name, user_nickname, user_password, user_email)
-        db_man.commit(query)
+        dbManager.commit(query)
     return(bool_ans)
 
 
@@ -35,18 +35,18 @@ def create_user(name,user_nickname,user_password,user_email):
 def update_user(id,user_name,user_nickname,user_email,user_password):
     if get_user_by_id(id)[4] == True:
         query = "UPDATE customers SET user_name = '%s', nickname= '%s',email= '%s',password='%s' WHERE id='%s';" % (user_name,user_nickname,user_email,user_password, id)
-        db_man.commit(query)
+        dbManager.commit(query)
         bool_ans=True
     else:
         bool_ans=False
-    updated_user=db_man.fetch("select * from customers where id='%s';" % id)
+    updated_user=dbManager.fetch("select * from customers where id='%s';" % id)
     return(updated_user,bool_ans)
 
 
 
 def delete_user(id):
     query="DELETE  FROM customers WHERE id='%s';" % id
-    db_man.commit(query)
+    dbManager.commit(query)
     check_if_user_exist="select * from customers where id='%s';"% id
     if check_if_user_exist == []:
         bool_ans= True
