62a99ed654dbbf1bb855ce8cc5df4decd7e18d34
noae123 <noae123@gmail.com>
2022-07-28 18:20:11 +0300

    creating card 4u client

--- web-project-g4/sql.sql
+++ web-project-g4/sql.sql
@@ -14,16 +14,16 @@ create table customers
 		check (length(`password`) >= 6)
 );
 
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (0, 'Card4You', 'card4u', 'CD123456', 'card4u@c4u.io');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (2, 'EDEN', 'ED77E77', '4567890', 'E@LJDN.T');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (4, 'Aviv', 'Aviv123', 'E&T22/08/22', 'aviv12@gmail.com');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (5, 'Yarden', 'Yardi90', 'Aa123456', 'yar@gmail.com');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (6, 'Noa', 'Nuni66', 'Nn123456', 'Noa@gmail.com');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (7, 'Gal', 'Galim15/08', 'Gg123456', 'Gal@gmail.com');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (8, 'Don', 'dondon5', 'Don12345', 'don@gmail.com');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (9, 'Tali', 'Talic536', 'Tt12345', 'tal@gmail.com');
-INSERT INTO `web-project-g4`.customers (id, user_name, nickname, password, email) VALUES (13, 'Roei', 'roei2107', 'Rr123456', 'Roei@gmail.com');
-
+insert into `web-project-g4`.customers (id, user_name, nickname, password, email)
+values  (1, 'Card4You', 'card4u', 'CD123456', 'card4u@c4u.io'),
+        (2, 'EDEN', 'ED77E77', '4567890', 'E@LJDN.T'),
+        (4, 'Aviv', 'Aviv123', 'E&T22/08/22', 'aviv12@gmail.com'),
+        (5, 'Yarden', 'Yardi90', 'Aa123456', 'yar@gmail.com'),
+        (6, 'Noa', 'Nuni66', 'Nn123456', 'Noa@gmail.com'),
+        (7, 'Gal', 'Galim15/08', 'Gg123456', 'Gal@gmail.com'),
+        (8, 'Don', 'dondon5', 'Don12345', 'don@gmail.com'),
+        (9, 'Tali', 'Talic536', 'Tt12345', 'tal@gmail.com'),
+        (13, 'Roei', 'roei2107', 'Rr123456', 'Roei@gmail.com');
 
 
 create table videos
@@ -48,9 +48,9 @@ create table videos
     constraint ck_max_sender
         check (octet_length(`sender_name`) <= 30)
 );
-
-INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (1, 2, null, null, 'eden', 'tami', 'red', 'triangle', null, 'var');
-INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (3, 4, null, null, 'Aviv', 'Tomi', 'green', 'squre', null, 'songi');
+INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (0, 0, 'happybday.mp3', 'card4you.png', 'card4you', 'happy birthday dear you!', 'blue', 'butterfly', null, 'Happy Birthday Example Card');
+INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (1, 2, null, null, 'eden', 'tami', 'red', 'circle', null, 'var');
+INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (3, 4, null, null, 'Aviv', 'Tomi', 'green', 'circle', null, 'songi');
 INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (4, 5, null, null, 'Dan', 'Hili', 'yellow', 'circle', null, 'stars');
-INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (5, 6, null, null, 'Yotam', 'Roni', 'blue', 'rectangle', null, 'moon');
-INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (6, 7, null, null, 'Yossi', 'Avi', 'orange', 'star', null, 'sunshine');
+INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (5, 6, null, null, 'Yotam', 'Roni', 'blue', 'circle', null, 'moon');
+INSERT INTO `web-project-g4`.videos (id_video, user_id, audio, image, sender_name, card_banner, color, shape, dir, video_name) VALUES (6, 7, null, null, 'Yossi', 'Avi', 'orange', 'circle', null, 'sunshine');
