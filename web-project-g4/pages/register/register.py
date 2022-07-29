1f019477cd7da4b5495b617b8629a47026245dd2
felzyard <102547325+felzyard@users.noreply.github.com>
2022-07-29 22:00:15 +0300

    login work, we need to do register

--- web-project-g4/pages/register/register.py
+++ web-project-g4/pages/register/register.py
@@ -10,12 +10,25 @@ register = Blueprint('register', __name__, static_folder='static', static_url_pa
 def index():
     return render_template('register.html')
 
-@register.route('/register', methods=['GET'])
+@register.route('/register/try_register', methods=['GET'])
 def try_register():
     print(request.form)
-    if (create_user(request.form['user_name'], request.form['nickname'], request.form['password'], request.form['email'])[1]):
-        #session['isRegister'] = True
-        # session['userId'] = get_id_by_user(request.form['user_name'])[]  todo eden this function
-        return redirect(url_for('login.index'))
-    else:
-        return redirect(url_for('register.index')) #todo think
+    if('inputUserName' in request.args or 'inputNickname' in request.args or 'inputPassword'  in request.args or 'inputEmail' in request.args):
+            password = request.args['inputPassword']
+            print(password)
+            confirm_password = request.args['inputConfirmPassword']
+            print(confirm_password)
+            user_name=request.args['inputUserName']
+            Nickname=request.args['inputNickname']
+            Email=request.args['inputEmail']
+            if (confirm_password == password):
+                if (create_user(user_name, Nickname, password,Email)[1]):
+                    massage='user was created'
+                    print(massage)
+                    return render_template('login.html' ,massage=massage)
+            else:
+                massage = 'wrong password'
+                print(massage)
+                return render_template('register.html',massage=massage)
+
+
